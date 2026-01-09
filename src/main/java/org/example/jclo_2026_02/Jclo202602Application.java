b25bd09e33b47bdba4309a0b4d7dc8453c8b669c
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-01-09 18:29:32 +0300

    Initial commit

---
+++ src/main/java/org/example/jclo_2026_02/Jclo202602Application.java
@@ -0,0 +1,13 @@
+package org.example.jclo_2026_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jclo202602Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jclo202602Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/jclo_2026_02/Jclo202602ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jclo_2026_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jclo202602ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
