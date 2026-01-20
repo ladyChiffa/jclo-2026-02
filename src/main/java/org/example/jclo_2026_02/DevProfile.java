bbc5f8ae90b8be1ad7924257be53293ba85e9fe3
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-01-20 23:31:30 +0300

    Настройка ELK контейнеров и передачи логов приложения в ELK.

--- src/main/java/org/example/jclo_2026_02/DevProfile.java
+++ src/main/java/org/example/jclo_2026_02/DevProfile.java
@@ -1,8 +1,12 @@
 package org.example.jclo_2026_02;
 
+import java.util.logging.Logger;
+
 public class DevProfile implements SystemProfile{
     @Override
     public String getProfile() {
+        System.out.println("Current profile is dev");
+        Logger.getLogger("system log").info("Current profile is dev");
         return "Current profile is dev";
     }
 }
--- src/main/java/org/example/jclo_2026_02/ProductionProfile.java
+++ src/main/java/org/example/jclo_2026_02/ProductionProfile.java
@@ -1,8 +1,12 @@
 package org.example.jclo_2026_02;
 
+import java.util.logging.Logger;
+
 public class ProductionProfile implements SystemProfile{
     @Override
     public String getProfile() {
+        System.out.println("Current profile is production");
+        Logger.getLogger("system log").info("Current profile is production");
         return "Current profile is production";
     }
 }
